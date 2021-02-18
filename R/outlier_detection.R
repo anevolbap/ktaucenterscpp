5c9a1857fb74062ebf35756eae6feb18317678ce
pablo <pvena1@gmail.com>
2021-02-18 14:05:29 -0300

    refactor thr argument

--- R/outlier_detection.R
+++ R/outlier_detection.R
@@ -1,14 +1,13 @@
-MSCALE_BREAKDOWN_POINT = 0.5
-
-flag_outliers <- function(ktau, cutoff) {
-  
-  thr <- qchisq(cutoff, df = ktau$p)
+outliers_tau_cutoff <- function(cutoff, mscale_bp) {
+  function(ktau) {
+    thr <- qchisq(cutoff, df = ktau$p)
+    robust_scale <- Mscale(u = ktau$di,
+                           b = mscale_bp,
+                           cc = normal_consistency_constants(ktau$p))
     
-  robust_scale <- Mscale(u = ktau$di,
-                         b = MSCALE_BREAKDOWN_POINT,
-                         cc = normal_consistency_constants(ktau$p))
+    ktau$outliers <- which(ktau$di ^ 2 > thr * robust_scale ^ 2)
+    
+    return(ktau)
+  }
   
-  ktau$outliers <- which(ktau$di^2 > thr * robust_scale^2)
-   
-  return(ktau)
 }
