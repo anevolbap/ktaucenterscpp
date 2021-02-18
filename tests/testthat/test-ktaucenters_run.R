90f5d4fee2b217a74e97d0df1c4145bf1d88e6c5
pablo <pvena1@gmail.com>
2021-02-18 12:58:26 -0300

    update tests

--- tests/testthat/test-ktaucenters_run.R
+++ tests/testthat/test-ktaucenters_run.R
@@ -1,4 +1,4 @@
-test_that("ktaucenters_aux", {
+test_that("ktaucenters_run", {
     set.seed(6)
     Z <- rnorm(600)
     mues <- rep(c(-4, 0, 4), 200)
@@ -6,7 +6,7 @@ test_that("ktaucenters_aux", {
     X[sample(1:300, 60), ] <- matrix(runif(40, 2 * min(X), 2 * max(X)),
                                      ncol = 2, nrow = 60)
     set.seed(6)
-    kaux_cpp = ktaucenterscpp::ktaucenters_aux(X, K = 4, centers = diag(4)[, 1:2],
+    kaux_cpp = ktaucenterscpp::ktaucenters_run(X, centers = diag(4)[, 1:2],
                               tolerance=1e-7,max_iter=1000)
 
     set.seed(6)
