f7b75f6262542c60d6465ebd299744f6fddfd7cc
pablo <pvena1@gmail.com>
2021-02-20 12:02:04 -0300

    replace "while" with "for" remove unnecesary "if"

--- R/robust_init_density.R
+++ R/robust_init_density.R
@@ -17,23 +17,23 @@
 #'
 #' @export
 #'
-#' @details 
-#'   This function do the same as ROBIN but taking into account the 
+#' @details
+#'   This function do the same as ROBIN but taking into account the
 #'   density (instead of the inverse of the average relative local
 #'   density known as LOF)
-#'   
+#'
 #'   The centers are the observations located in the most dense
 #'     region and far away from each other at the same time.  In order
 #'     to find the observations in the highly dense region,
 #'     ROBINPOINTDEN uses point density estimation (instead of Local
 #'     Outlier Factor, Breunig et al. (2000)), see more details.
-#'     
+#'
 #'     Observation: Outliers have a high 'idp' value. In imbalanced cases
 #'     and when K increases, all the observations from a group might be
 #'     above the critRobin, So we need to increase the critRobin in order
 #'     to avoid two initials centers from the same group.
 #'     modification: start with a point whose density is maximum
-#'     
+#'
 #' @examples
 # Generate Synthetic data (7 cluster well separated)
 #' K=5;
@@ -62,45 +62,35 @@
 #'     Letters, 30(11), 994-1002, 2009.
 #'
 #' @seealso \code{\link[dbscan]{lof}}
-#'
-#' @importFrom dbscan lof
-#' @importFrom dbscan kNN
-#
-robust_init_density <- function(dist_matrix, data, n_clusters, 
-                                mp = 10, method = "density") {
-  
-  id_means <- numeric(n_clusters)
-  iter <- 1  
-  n <- nrow(data)
+robust_init_density <- function(dist_matrix,
+                                data,
+                                n_clusters,
+                                mp = 10,
+                                method = "density") {
   
   # Compute the inverse density points.
-  idp <- density_points(dist_matrix, k = mp)
-  position <- floor(max(0.5, 0.96 * (1 - 1.5 / n_clusters)) * n)
+  idp <- density_points(dist_matrix, neigh = mp)
+  position <- floor(max(0.5, 0.96 * (1 - 1.5 / n_clusters)) * nrow(data))
   crit_robin <- sort(idp)[position]
   
   if (method == "robin") r <- sample(n, 1)
   if (method == "density") r <- which.min(idp)
   
-  dist_matrix = as.matrix(dist_matrix) # FIXME: check types 
-  while(iter <= n_clusters){
-    if (iter <= 2) {
-      sorted_points <- order(dist_matrix[r, ], decreasing = TRUE)
-    } else {
-      sorted_points <- order(apply(dist_matrix[id_means[1:iter], ], 2, min), #FIXME: try Rfast package
-                             decreasing = TRUE)
-    }
+  dist_matrix <- as.matrix(dist_matrix)
+  id_means <- numeric(n_clusters)
+  id_means[1] =  r
+  for (iter in 1:n_clusters) {
+    points <- apply(dist_matrix[id_means[1:iter], , drop = FALSE], 2, min)
+    sorted_points <- order(points, decreasing = TRUE)
     idp_sorted_points <- idp[sorted_points]
     
-    id <- ifelse(any(idp_sorted_points <= crit_robin),
-                 which(idp_sorted_points <= crit_robin)[1], # FIXME: efficient "first true"
-                 which.min(idp_sorted_points - crit_robin, decreasing))
+    idx <- ifelse(any(idp_sorted_points <= crit_robin),
+                  which.max(idp_sorted_points <= crit_robin),
+                  which.min(idp_sorted_points - crit_robin))
     
-    r <- sorted_points[id]
+    r <- sorted_points[idx]
     id_means[iter] <- r
-    
-    iter <- iter + 1
   }
   
-  return(list(centers = id_means,
-              id_points = idp))
+  return(list(centers = id_means, id_points = idp))
 }
